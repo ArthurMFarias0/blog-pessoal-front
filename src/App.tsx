508ea17b06495d89a35e4773094875a58d5955c8
Arthur <arthuremeunome@gmail.com>
2022-11-29 14:20:15 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/estaticos/navbar/Navbar';
+import { Home } from './components/paginas/home/Home';
+import { About } from './components/paginas/about/About';
 
 function App() {
+  const nome = 'Arthur';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/paginas/about/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './About.css'
+
+function About () {
+    return (
+        <h1>Página About</h1>
+    )
+}
+
+export { About }
