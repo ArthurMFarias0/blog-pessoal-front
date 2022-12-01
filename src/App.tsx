3a802e42d59d934a8a88f88f613996d38ff7de5f
Arthur <arthuremeunome@gmail.com>
2022-12-01 16:03:01 -0300

    add: implementação de Grid e Box

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,37 @@
 import './App.css';
 import React from "react";
+import { Grid } from '@material-ui/core';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/estaticos/navbar/Navbar';
 import { Home } from './components/paginas/home/Home';
-import { About } from './components/paginas/about/About';
 
 function App() {
   const nome = 'Arthur';
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-      </Routes>
-    </Router>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12}>
+          <Home/>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home/>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Home/>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home/>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home/>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home/>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Home/>
+        </Grid>
+      </Grid>
     </>
   );
 }
--- src/components/paginas/home/Home.tsx
+++ src/components/paginas/home/Home.tsx
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './Home.css'
+import { Box, Button, Grid, Paper } from "@material-ui/core";
 
 function Home () {
-    const [num, setNum] = useState(0);
-
+    
     return (
         <>
-        <h1>Você clicou {num} vezes</h1>
-        <button onClick={() => setNum(num + 1)}>
-            Clique aqui
-        </button>
+            <Paper>
+                <Box p={2}>
+                    <Box display="flex" justifyContent="center">
+                        <h1>Título</h1>
+                    </Box>
+                    <img src="https://images-ext-2.discordapp.net/external/vHBEfQIIU075-tdFDZUiSl6JnG4y2XvQXmxRB-l_nk8/%3Fs%3D280%26v%3D4/https/avatars.githubusercontent.com/u/115719526" alt="" style={{width: "80%", height: "80%"}} />
+                    <Box display="flex" justifyContent="center" p={2}>
+                        <Button variant="contained" color="primary">Texto 1</Button>
+                        <Button variant="contained" color="secondary">Texto 2</Button>
+                    </Box>
+                </Box>
+            </Paper>
         </>
     );
 }
